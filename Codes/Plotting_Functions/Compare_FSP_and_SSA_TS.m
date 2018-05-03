d6e12828529fd414c3c33bf35e7ac21db276e8b1
Brian Munsky <Munsky@users.noreply.github.com>
2018-05-03 15:48:31 -0600

    Added warning and download link for missing data file.

--- Codes/Plotting_Functions/Compare_FSP_and_SSA_TS.m
+++ Codes/Plotting_Functions/Compare_FSP_and_SSA_TS.m
@@ -24,68 +24,83 @@ Sp_Type = 'Spatial';
 speed = 63; % CTT1  - mn of on TS.
 clear TMP TS_All
 for ig=1:2
-switch ig
-    case 1
-        gene = 'STL1';
-        PD = PD_STL1;
-        len = 1630;
-    case 2
-        gene = 'CTT1';
-        PD = PD_CTT1;
-        len  = 1618;
-end
-
-for i_nacl = 1:2
-    TS_All(i_nacl).Data = [];
-        fil = ['Fit_Results/',gene,'_0p2and0p4/',Sp_Type,'/Distributions/4State_k21only_noloop/Analyses.mat'];
-        load(fil)
-        MOD_FSP = Hog.MOD;
-    switch i_nacl
+    switch ig
         case 1
-            n_reps = 2;
-            nacl = '0p2NaCl';
-                MOD_FSP.Salt = 0.2;
-            dat_col = [0.8 0 0.8];
-            mod_col = [0 0 0];
-            fil = ['Fit_Results/SSATS_',gene,'_Distributions_0.2_',Sp_Type,'.mat'];
+            gene = 'STL1';
+            PD = PD_STL1;
+            len = 1630;
         case 2
-            n_reps = 3;
-            nacl = '0p4NaCl';
-            dat_col = [0 0.8 0];
+            gene = 'CTT1';
+            PD = PD_CTT1;
+            len  = 1618;
+    end
+    
+    for i_nacl = 1:2
+        TS_All(i_nacl).Data = [];
+        fil = ['Fit_Results/',gene,'_0p2and0p4/',Sp_Type,'/Distributions/4State_k21only_noloop/Analyses.mat'];
+        try
+            load(fil)
+        catch
+            disp('WARNING - Missing data file named:')
+            fil
+            if strcmp(fil,'Fit_Results/CTT1_0p2and0p4/Spatial/Distributions/4State_k21only_noloop/Analyses.mat')
+                disp('You can download this file at:');
+                disp('https://www.dropbox.com/s/g4sygvsh8piq0jp/Analyses.mat?dl=0')
+                disp('and move it to the directory:')
+                disp('Fit_Results/CTT1_0p2and0p4/Spatial/Distributions/4State_k21only_noloop/')
+                disp('or you can regenerate this and all file susing the button at the top-right.')
+            else
+                disp('You can regenerate this and all file susing the button at the top-right')
+            end
+            return
+       end
+        MOD_FSP = Hog.MOD;
+        switch i_nacl
+            case 1
+                n_reps = 2;
+                nacl = '0p2NaCl';
+                MOD_FSP.Salt = 0.2;
+                dat_col = [0.8 0 0.8];
+                mod_col = [0 0 0];
+                fil = ['Fit_Results/SSATS_',gene,'_Distributions_0.2_',Sp_Type,'.mat'];
+            case 2
+                n_reps = 3;
+                nacl = '0p4NaCl';
+                dat_col = [0 0.8 0];
                 MOD_FSP.Salt = 0.4;
-            mod_col = [0 0 0.4];
-            fil = ['Fit_Results/SSATS_',gene,'_Distributions_0.4_',Sp_Type,'.mat'];
+                mod_col = [0 0 0.4];
+                fil = ['Fit_Results/SSATS_',gene,'_Distributions_0.4_',Sp_Type,'.mat'];
+        end
+        
+        %% Compute FSP analysis of TS intensity
+        MOD_FSP.Fit_Type='Distributions_TS';
+        MOD_FSP.Type = 'Distributions_TS';
+        MOD_FSP.k_elong = speed;
+        MOD_FSP.gene_length=len;
+        MOD_FSP.TS_Lim = 100;
+        Distributions_TS = Hog_Model.solve_hog(MOD_FSP);
+        load('Part_Intens.mat','Partial_Intens_Map');
+        C = ones(1,N*10+1);
+        Ci = ones(1,10);
+        for j=N+1:MOD_FSP.TS_Lim
+            C = sparse(blkdiag(C,Ci));
+        end
+        Partial_Intens_Map_Red = C*Partial_Intens_Map(1:MOD_FSP.TS_Lim*10+1,1:MOD_FSP.TS_Lim+1);
+        inten_dist = Partial_Intens_Map_Red*Distributions_TS.distributions(1:MOD_FSP.TS_Lim+1,:);
+        
+        %%
+        TS_all_reps=[];
+        f2=figure(100*(ig-1)+10*(i_nacl-1)+2);%subplot(3,1,1);
+        f3=figure(100*(ig-1)+10*(i_nacl-1)+3);
+        f4=figure(100*(ig-1)+14+i_nacl);
+        N_cells = 1000*ones(1,16);
+        Means_Active_TS_Mod(ig,i_nacl)=Model_TS_Results([],MOD_FSP,[],PD,N,f2,f3,f4,mod_col,max(N_cells),N_cells,inten_dist,1212);
+        
+        load(fil,'MOD','SSA')
+        for i=1:5
+            Model_TS_Results_SSA(SSA,MOD,speed,PD,N,f2,f3,f4,mod_col,max(N_cells),N_cells,inten_dist);
+        end
     end
-    
-    %% Compute FSP analysis of TS intensity
-    MOD_FSP.Fit_Type='Distributions_TS';
-    MOD_FSP.Type = 'Distributions_TS';
-    MOD_FSP.k_elong = speed;
-    MOD_FSP.gene_length=len;
-    MOD_FSP.TS_Lim = 100;
-    Distributions_TS = Hog_Model.solve_hog(MOD_FSP);
-    load('Part_Intens.mat','Partial_Intens_Map');
-    C = ones(1,N*10+1);
-    Ci = ones(1,10);
-    for j=N+1:MOD_FSP.TS_Lim
-        C = sparse(blkdiag(C,Ci));
-    end
-    Partial_Intens_Map_Red = C*Partial_Intens_Map(1:MOD_FSP.TS_Lim*10+1,1:MOD_FSP.TS_Lim+1);
-    inten_dist = Partial_Intens_Map_Red*Distributions_TS.distributions(1:MOD_FSP.TS_Lim+1,:);
-    
-    %%
-    TS_all_reps=[];
-    f2=figure(100*(ig-1)+10*(i_nacl-1)+2);%subplot(3,1,1);
-    f3=figure(100*(ig-1)+10*(i_nacl-1)+3);
-    f4=figure(100*(ig-1)+14+i_nacl);
-    N_cells = 1000*ones(1,16);
-    Means_Active_TS_Mod(ig,i_nacl)=Model_TS_Results([],MOD_FSP,[],PD,N,f2,f3,f4,mod_col,max(N_cells),N_cells,inten_dist);
-
-    load(fil,'MOD','SSA')
-    for i=1:5
-        Model_TS_Results_SSA(SSA,MOD,speed,PD,N,f2,f3,f4,mod_col,max(N_cells),N_cells,inten_dist);
-    end
-end
 end
 
 %%
@@ -97,10 +112,10 @@ for i=[15 16 115 116]
 end
 
 for i=[2 12 102 112]
-set(i,'position',[1245         809         274         161]);
-figure(i);set(gca,'fontsize',14,'yscale','linear','xlim',[0,60])
-ylabel('')
-xlabel('')
+    set(i,'position',[1245         809         274         161]);
+    figure(i);set(gca,'fontsize',14,'yscale','linear','xlim',[0,60])
+    ylabel('')
+    xlabel('')
 end
 
 
